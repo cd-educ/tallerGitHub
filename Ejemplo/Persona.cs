80b39b473ec542a994521c8838d56e6d328f3ce0
CDNievas <cdnievas@hotmail.com>
2019-04-13 09:24:17 -0300

    Funcionalidad energia

---
+++ Ejemplo/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejemplo
+{
+    public class Persona
+    {
+
+        public string nombre;
+        public int edad;
+        public int energia = 100;
+
+        public Persona(string unNombre, int unaEdad)
+        {
+            this.nombre = unNombre;
+            this.edad = unaEdad;
+        }
+
+        public void dormir(int energiaGanada)
+        {
+            energia = Math.Min(100, energia + energiaGanada);
+        }
+
+        public void hacerEjercicio(int energiaGastada)
+        {
+            energia = Math.Max(0, energia - energiaGastada);
+        }
+
+        public void verEnergia()
+        {
+            Console.WriteLine("Energia: " + this.energia);
+        }
+
+    }
+}
--- Ejemplo/Program.cs
+++ Ejemplo/Program.cs
@@ -6,7 +6,21 @@ namespace Ejemplo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            Persona p = new Persona("Jorge Rodriguez", 30);
+
+            // Jorge hace ejercicio gastando 70 de energia
+            p.hacerEjercicio(70);
+
+            // Jorge duerme un par de horas recuperando 30 de energia
+            p.dormir(30);
+
+            // Muestra la energia de Jorge
+            p.verEnergia();
+
+            Console.ReadLine();
+
         }
     }
 }
