a7763282538cab3ce6e3b72e2d26c921026c8532
CDNievas <cdnievas@hotmail.com>
2019-04-13 09:19:18 -0300

    Funcionalidad Saldo

---
+++ Ejemplo/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejemplo
+{
+    public class Persona
+    {
+
+        public string nombre;
+        public int edad;
+        public float saldo = 0;
+
+        public Persona(string unNombre, int unaEdad)
+        {
+            this.nombre = unNombre;
+            this.edad = unaEdad;
+        }
+
+        public void cobrar(float dinero)
+        {
+            saldo = saldo + dinero;
+        }
+
+        public void pagar(float dinero)
+        {
+            saldo = saldo - dinero;
+        }
+
+        public void verSaldo()
+        {
+            Console.WriteLine("Saldo: " + this.saldo);
+        }
+
+    }
+}
--- Ejemplo/Program.cs
+++ Ejemplo/Program.cs
@@ -6,7 +6,21 @@ namespace Ejemplo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Persona p = new Persona("Pepito Flores", 67);
+
+            // Pepito cobra la jubilacion
+            p.cobrar(8000);
+
+            // Pepito paga Netflix
+            p.pagar(500);
+
+            // Muestra el saldo restante de pepito
+            p.verSaldo();
+
+            // Esto hace que no se cierra la consola para poder leer lo que se imprime
+            Console.ReadLine();
+
         }
     }
 }
